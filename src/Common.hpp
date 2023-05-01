287b03520dd7009348a59a5a35dd27b66223adeb
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2023-05-01 13:39:45 +0100

    Add some helpers for random numbers

--- src/Common.hpp
+++ src/Common.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 using std::shared_ptr;
 using std::make_shared;
@@ -16,6 +17,19 @@ const double PI = 3.1415926535897932385;
 inline double degToRad(double degrees) {
     return degrees * PI / 180.0;
 }
+
+inline double randomDouble(double min = 0.0, double max = 1.0) {
+    static std::uniform_real_distribution<double> distribution(min, max);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline int randomInt(int min = 0, int max = 255) {
+    static std::uniform_int_distribution<int> distribution(min, max);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
 
 // Common Headers
 #include "Ray.hpp"
